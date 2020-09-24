00912598b602f87b0930cd772713af07343e0661
AntoninaSeleva <49241471+AntoninaSeleva@users.noreply.github.com>
2020-09-24 16:26:14 +0300

    Update conftest.py
    Обновила по рецензии конф.тест

--- Week4/1/conftest.py
+++ Week4/1/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
 	parser.addoption('--language', action='store', default="ru",\
@@ -13,8 +14,12 @@ def language(request):
 
 	
 @pytest.fixture(scope="function")
-def browser():
-	browser = webdriver.Chrome()
+def browser(language):
+
+	options = Options()
+	options.add_experimental_option('prefs', {'intl.accept_languages': language})
+	browser = webdriver.Chrome(options=options)	
+	
 	browser.implicitly_wait(5)
 	browser.maximize_window ()
 	yield browser
